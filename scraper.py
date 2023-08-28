7fa9f3d4cd617b9852be939e7b31fb69aff6115f
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-08-28 10:13:36 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 import requests
 import json
 import sys
+import gzip
 
 if __name__ == "__main__":
 
+    res = requests.get(sys.argv[1]).json()
     with open('rules.json', 'w', encoding='utf-8') as f:
-        res = requests.get(sys.argv[1]).json()
         json.dump(res, f)
+    with gzip.open('rules.gzip', 'wb') as gf:
+        gf.write(gzip.compress(bytes(json.dumps(res), 'utf-8')))
